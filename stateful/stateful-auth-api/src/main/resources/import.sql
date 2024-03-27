a5147130b44c56ecbcdf967866a57ebb951721af
focaG <60098824+Edamatec@users.noreply.github.com>
2024-03-27 14:20:31 -0300

    Feat: Conectando os microsserviços no banco

---
+++ stateful/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user (id, username, password) VALUES (1, 'admin.teste', '$2a$12$wP52Ba6vWfkPIlV4NwJOkeo6dglyexFcufjvFgCtFQEhIKCDNnpKy');
+INSERT INTO public.user (id, username, password) VALUES (2, 'novo.teste', '$2a$12$wP52Ba6vWfkPIlV4NwJOkeo6dglyexFcufjvFgCtFQEhIKCDNnpKy');
